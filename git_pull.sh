6e94df49d8500b5cd79fe75cec05dc789ed3bc21
q2434006 <243400623@qq.com>
2021-01-30 10:47:30 +0800

    Update git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -32,11 +32,11 @@ WhichDep=$(grep "/jd-base" "${ShellDir}/.git/config")
 Scripts2URL=https://github.com/shylocks/Loon
 
 if [[ ${WhichDep} == *github* ]]; then
-  ScriptsURL=https://github.com/LXK9301/jd_scripts
-  ShellURL=https://github.com/EvineDeng/jd-base
+  ScriptsURL=https://gitee.com/lxk0301/jd_scripts
+  ShellURL=https://github.com/q2434006/jd-base
 else
   ScriptsURL=https://gitee.com/lxk0301/jd_scripts
-  ShellURL=https://gitee.com/evine/jd-base
+  ShellURL=https://github.com/q2434006/jd-base
 fi
 
 ## 更新shell脚本
